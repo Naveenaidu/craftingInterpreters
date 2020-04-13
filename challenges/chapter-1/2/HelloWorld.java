f81a1302c65197d0e6891dd1466266f123fbb3e3
Naveen Naidu <naveennaidu479@gmail.com>
2020-04-13 19:01:27 +0530

    ch_1/chal_2: Solution

---
+++ challenges/chapter-1/2/HelloWorld.java
@@ -0,0 +1,9 @@
+/*
+ * A sample Hello world program to brush up java concpets and set up environment 
+ * for development
+ */
+class HelloWorld{
+	public static void main(String[] args){
+		System.out.println("Hiya Homosapiens");
+	}
+}
